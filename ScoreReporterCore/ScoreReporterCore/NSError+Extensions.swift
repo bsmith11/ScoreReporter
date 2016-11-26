1ea28ab20676f8eed7279977030cc62ded27aa68
Brad Smith <bradley.d.smith11@gmail.com>
2016-11-25 22:35:07 -0500

    Added custom error handling

---
+++ ScoreReporterCore/ScoreReporterCore/NSError+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  NSError+Extensions.swift
+//  ScoreReporterCore
+//
+//  Created by Bradley Smith on 11/25/16.
+//  Copyright © 2016 Brad Smith. All rights reserved.
+//
+
+import Foundation
